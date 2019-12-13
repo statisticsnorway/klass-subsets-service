c8208d2436451181f4f1c0ca3e2c307e52b64637
kons_lapi <alina.lapina@ssb.no>
2019-12-13 13:35:12 +0100

    log setup @rama41222/node-logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 
 const config = {
     name: "klass-subsets-service",
@@ -8,11 +9,13 @@ const config = {
 };
 
 const app = express();
+const logger = log({ console: true, file: false, label: config.name });
 app.use(bodyParser.json());
 app.use(cors());
+app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 app.get("/", (req, res) => {
     res.status(200).send("klass subsets service is running");
 });
 
-app.listen(config.port, ()=> console.log(`${config.name} listening on port ${config.port}`));
+app.listen(config.port, ()=> logger.info(`${config.name} listening on port ${config.port}`));
