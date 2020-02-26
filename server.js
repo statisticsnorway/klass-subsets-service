f2e74db725b3425af2acd58291e719fd5c8d5e51
alina-lapina <Zaq12wsx->
2020-02-26 13:33:25 +0100

    test data from a file

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
+const data = require('./src/test/subsets.json');
 const subsetsRouter = express.Router();
 
 subsetsRouter.route('/subsets')
     .get((req, res) => {
-      const subsets = [{id: 1, name: "The very first subset"}, {id: 2, name: "Second subset"}];
-      res.json(subsets);
+      res.json(data);
     });
 app.use('/api', subsetsRouter);
 
 app.get('/', (req, res) => {
-  res.status(200).send('klass subsets service is running');
+  res.status(200).send('klass subsets service is running4');
 });
 
 app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
