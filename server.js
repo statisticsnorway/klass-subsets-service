86462560cd43e68f92de120e1f60be714a23a46c
alina-lapina <Zaq12wsx->
2020-02-26 12:15:52 +0100

    eslint installed and configured

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 
 const config = {
-    name: "klass-subsets-service",
-    port: 5000
+  name: 'klass-subsets-service',
+  port: 5000
 };
 
 const app = express();
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-app.get("/", (req, res) => {
-    res.status(200).send("klass subsets service is running");
+app.get('/', (req, res) => {
+  res.status(200).send('klass subsets service is running');
 });
 
-app.listen(config.port, ()=> logger.info(`${config.name} listening on port ${config.port}`));
+app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
