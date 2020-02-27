b6791ec6c6fbba6bd4e8e298002852b447eaaf15
alina-lapina <Zaq12wsx->
2020-02-27 06:38:21 +0100

    /subsets/:id placeholder

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-const data = require('./src/test/subsets.json');
+const fs = require('fs');
+const raw = fs.readFileSync('./src/test/subsets.json');
+const data = JSON.parse(raw);
 const subsetsRouter = express.Router();
 
-subsetsRouter.route('/subsets')
-    .get((req, res) => {
-      res.json(data);
-    });
+subsetsRouter.route('/subsets').get((req, res) => {res.json(data);});
+subsetsRouter.route('/subsets/:id').get((req, res) => {res.json( data.find(i => i.id == req.params.id));});
 app.use('/api', subsetsRouter);
 
 app.get('/', (req, res) => {
-  res.status(200).send('klass subsets service is running4');
+  res.status(200).send('klass subsets service is running');
 });
 
 app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
