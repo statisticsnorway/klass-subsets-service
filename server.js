6f3bf743ba5ee0d154d8f8a25aaeffae2cd3c7c0
kons_lapi <alina.lapina@ssb.no>
2020-02-27 12:13:39 +0100

    /subsets post placeholder

--- server.js
+++ server.js
@@ -2,29 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+const fs = require('fs');
 
 const config = {
   name: 'klass-subsets-service',
   port: 5000
 };
 
-const app = express();
 const logger = log({ console: true, file: false, label: config.name });
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-const fs = require('fs');
-const raw = fs.readFileSync('./src/test/subsets.json');
-const data = JSON.parse(raw);
+app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.2 is running'));
+
+const data = JSON.parse(fs.readFileSync('./src/test/subsets.json'));
 const subsetsRouter = express.Router();
 
-subsetsRouter.route('/subsets').get((req, res) => {res.json(data);});
-subsetsRouter.route('/subsets/:id').get((req, res) => {res.json( data.find(i => i.id == req.params.id));});
-app.use('/api', subsetsRouter);
+app.use('/subsets', subsetsRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).send('klass subsets service is running');
-});
+subsetsRouter.route('/')
+    .get((req, res) => res.status(200).json(data))
+    .post((req, res) => res.status(200).json(req.body));
+
+subsetsRouter.route('/:id')
+    .get((req, res) => res.status(200).json(data.find(i => i.id == req.params.id)));
 
 app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
