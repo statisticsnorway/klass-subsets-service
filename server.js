9d6bb78ac173fde46e4519e6788a46fc85934f89
kons_lapi <alina.lapina@ssb.no>
2020-03-24 21:09:54 +0100

    refactored: decomposed subsetsRouter into a separate file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('./src/test/subsets.json'));
+const subsetsRouter = require('./src/routes/subsetsRouter')(data);
 
 const config = {
   name: 'klass-subsets-service',
@@ -17,6 +19,9 @@ const logger = log({ console: true, file: false, label: config.name });
 app.use(ExpressAPILogMiddleware(logger, { request: true, response: true }));
 
 app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.5 is running'));
+app.use('/api', subsetsRouter);
+app.use('/auth', subsetsRouter);
+app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.5 is running'));
 
 app.get('/klass-api', (req, res) => {
     fetch('https://data.ssb.no/api/klass/v1/classifications/68/codesAt.json?date=2020-03-18&selectCodes=1')
@@ -42,30 +47,5 @@ app.get('/lds-klass/subset1', (req, res) => {
         .then(subset_data => res.status(200).json(subset_data))
         .catch(err => console.error(err));
 });
-
-app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.5 is running'));
-
-const data = JSON.parse(fs.readFileSync('./src/test/subsets.json'));
-const subsetsRouter = express.Router();
-
-app.use('/api', subsetsRouter);
-app.use('/auth', subsetsRouter);
-
-subsetsRouter.route('/v1/subsets')
-    .get((req, res) =>
-        res.status(200).json(data))
-    .post((req, res) => {
-      const subset = req.body;
-      subset.id = data.length+1;
-      data.push(subset);
-      res.status(200).json(subset);
-    })
-    .put((req, res) => {
-        data[data.findIndex(i => i.id == req.body.id)] = req.body;
-        res.status(200).json(data.find(i => i.id = req.body.id));
-    });
-
-subsetsRouter.route('/v1/subsets/:id')
-    .get((req, res) => res.status(200).json(data.find(i => i.id == req.params.id)));
 
 app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
---
+++ src/routes/subsetsRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+function routes(data) {
+    const subsetsRouter = express.Router();
+    subsetsRouter.route('/v1/subsets')
+        .get((req, res) =>
+            res.status(200).json(data))
+        .post((req, res) => {
+            const subset = req.body;
+            subset.id = data.length+1;
+            data.push(subset);
+            res.status(200).json(subset);
+        })
+        .put((req, res) => {
+            data[data.findIndex(i => i.id == req.body.id)] = req.body;
+            res.status(200).json(data.find(i => i.id = req.body.id));
+        });
+
+    subsetsRouter.route('/v1/subsets/:id')
+        .get((req, res) => res.status(200).json(data.find(i => i.id == req.params.id)));
+
+    return subsetsRouter;
+}
+
+module.exports = routes;
