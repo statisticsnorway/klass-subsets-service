fd9ddea4bcdbc4c37e259e4101b26b30b7af443f
kons_lapi <alina.lapina@ssb.no>
2020-03-24 15:40:47 +0100

    v0.1.5 lds-klass post subsets1

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 const logger = log({ console: true, file: false, label: config.name });
 app.use(ExpressAPILogMiddleware(logger, { request: true, response: true }));
 
-app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.4 is running'));
+app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.5 is running'));
 
 app.get('/klass-api', (req, res) => {
     fetch('https://data.ssb.no/api/klass/v1/classifications/68/codesAt.json?date=2020-03-18&selectCodes=1')
@@ -24,13 +24,26 @@ app.get('/klass-api', (req, res) => {
         .then(klass_data => res.status(200).json(klass_data));
 });
 
-app.get('/lds-klass', (req, res) => {
-    fetch('http://lds-klass.klass.svc.cluster.local/ns/Agent/?schema ')
+app.get('/lds-klass/schema', (req, res) => {
+    fetch('http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset/?schema ')
         .then(response => response.json())
         .then(lds_data => res.status(200).json(lds_data));
 });
 
-app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.4 is running'));
+const subset1 = JSON.parse(fs.readFileSync('./src/test/subset1.json'));
+app.get('/lds-klass/subset1', (req, res) => {
+
+    fetch('http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset/1', {
+        method: 'post',
+        body: JSON.stringify(subset1),
+        headers: {'Content-Type': 'application/json'},
+    })
+        .then(response => response.json())
+        .then(subset_data => res.status(200).json(subset_data))
+        .catch(err => console.error(err));
+});
+
+app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.5 is running'));
 
 const data = JSON.parse(fs.readFileSync('./src/test/subsets.json'));
 const subsetsRouter = express.Router();
