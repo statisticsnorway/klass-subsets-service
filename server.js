3b2c29fea77adf832954fcca82f3aaa9f6bcfd96
kons_lapi <alina.lapina@ssb.no>
2020-03-24 23:30:30 +0100

    v0.1.6 refactored: decomposed to different files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
-const fetch = require('node-fetch');
+const {log, ExpressAPILogMiddleware} = require('@rama41222/node-logger');
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./src/test/subsets.json'));
 const subsetsRouter = require('./src/routes/subsetsRouter')(data);
+const lds = require('./src/services/lds-api');
+const klass = require('./src/services/klass-api');
 
 const config = {
   name: 'klass-subsets-service',
@@ -18,34 +19,28 @@ app.use(cors());
 const logger = log({ console: true, file: false, label: config.name });
 app.use(ExpressAPILogMiddleware(logger, { request: true, response: true }));
 
-app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.5 is running'));
+app.get('/', (req, res) => res.status(200).send('klass subsets service v0.1.6 is running'));
 app.use('/api', subsetsRouter);
 app.use('/auth', subsetsRouter);
-app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.5 is running'));
+app.get('/auth', (req, res) => res.status(200).send('AUTHORIZED: klass subsets service v0.1.6 is running'));
 
 app.get('/klass-api', (req, res) => {
-    fetch('https://data.ssb.no/api/klass/v1/classifications/68/codesAt.json?date=2020-03-18&selectCodes=1')
-        .then(response => response.json())
-        .then(klass_data => res.status(200).json(klass_data));
+    klass.getCode()
+        .then(code => res.status(200).json(code))
+        .catch(err => console.error(err));
 });
 
 app.get('/lds-klass/schema', (req, res) => {
-    fetch('http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset/?schema ')
-        .then(response => response.json())
-        .then(lds_data => res.status(200).json(lds_data));
+    lds.getSchema()
+        .then(schema => res.status(200).json(schema))
+        .catch(err => console.error(err));
 });
 
 const subset1 = JSON.parse(fs.readFileSync('./src/test/subset1.json'));
 app.get('/lds-klass/subset1', (req, res) => {
-
-    fetch('http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset/1', {
-        method: 'post',
-        body: JSON.stringify(subset1),
-        headers: {'Content-Type': 'application/json'},
-    })
-        .then(response => response.json())
+    lds.postSubset(subset1)
         .then(subset_data => res.status(200).json(subset_data))
-        .catch(err => console.error(err));
+        .catch(err => console.error(err))
 });
 
 app.listen(config.port, () => logger.info(`${config.name} listening on port ${config.port}`));
